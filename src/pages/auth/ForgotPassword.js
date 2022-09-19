90f670ec609a5bb3119ea6351a9893c0796458d2
Bhupendra <bhupendra.kumar@liqvid.com>
2022-09-19 20:28:00 +0530

    code clean

--- src/pages/auth/ForgotPassword.js
+++ src/pages/auth/ForgotPassword.js
@@ -11,31 +11,24 @@ const ForgotPassword = () => {
         phoneNumber: "",
     });
     return (
-        <>
-            <Box>
-                <Grid container>
-                    <Grid xs={12} >
-                        <Typography variant='h3'><Box mb={3} mt={9}>Forgot Password?</Box></Typography>
-                    </Grid>
-                    <Grid xs={12}>
-                        <Typography variant='body2'><Box mb={3}>No worries, You can reset your password by verifying your Phone Number with an OTP.</Box></Typography>
-                    </Grid>
-                    <Grid xs={12}>
-                        <Box mb={3} width={1}>
-                            <CustomTextField placeholder="Phone Number" value={pageData.phoneNumber} type="tel" variant="filled"
-                                onChange={(event) => {
-                                    setPageData({ ...pageData, phoneNumber: event.target.value })
-                                }
-                                }
-                            />
-                        </Box>
-                    </Grid>
-                    <Grid xs={12}>
-                        <CustomButton btnText="Next" color="primary" variant="contained" onClick={submitHandler} />
-                    </Grid>
+        <Box>
+            <Grid container>
+                <Grid xs={12} >
+                    <Typography variant='h3'><Box mb={3} mt={9}>Forgot Password?</Box></Typography>
                 </Grid>
-            </Box>
-        </>
+                <Grid xs={12}>
+                    <Typography variant='body2'><Box mb={3}>No worries, You can reset your password by verifying your Phone Number with an OTP.</Box></Typography>
+                </Grid>
+                <Grid xs={12}>
+                    <Box mb={3} width={1}>
+                        <CustomTextField placeholder="Phone Number" value={pageData.phoneNumber} type="tel" variant="filled" onChange={(event) => {setPageData({ ...pageData, phoneNumber: event.target.value })}}/>
+                    </Box>
+                </Grid>
+                <Grid xs={12}>
+                    <CustomButton btnText="Next" color="primary" variant="contained" onClick={submitHandler} />
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 export default ForgotPassword;
--- src/pages/auth/SignUp.js
+++ src/pages/auth/SignUp.js
@@ -11,31 +11,24 @@ const SignUp = () => {
         phoneNumber: "",
     });
     return (
-        <>
-            <Box>
-                <Grid container>
-                    <Grid xs={12} >
-                        <Typography variant='h3'><Box mb={3} mt={9}>Sign Up</Box></Typography>
-                    </Grid>
-                    <Grid xs={12}>
-                        <Typography variant='body2'><Box mb={3}>Existing User? Enter your login details below,</Box></Typography>
-                    </Grid>
-                    <Grid xs={12}>
-                        <Box mb={3} width={1}>
-                            <CustomTextField placeholder="Phone Number" value={pageData.phoneNumber} type="tel" variant="filled"
-                                onChange={(event) => {
-                                    setPageData({ ...pageData, phoneNumber: event.target.value })
-                                }
-                                }
-                            />
-                        </Box>
-                    </Grid>
-                    <Grid xs={12}>
-                        <CustomButton btnText="Sign Up" color="primary" variant="contained" onClick={submitHandler} />
-                    </Grid>
+        <Box>
+            <Grid container>
+                <Grid xs={12} >
+                    <Typography variant='h3'><Box mb={3} mt={9}>Sign Up</Box></Typography>
                 </Grid>
-            </Box>
-        </>
+                <Grid xs={12}>
+                    <Typography variant='body2'><Box mb={3}>Existing User? Enter your login details below,</Box></Typography>
+                </Grid>
+                <Grid xs={12}>
+                    <Box mb={3} width={1}>
+                        <CustomTextField placeholder="Phone Number" value={pageData.phoneNumber} type="tel" variant="filled" onChange={(event) => {setPageData({ ...pageData, phoneNumber: event.target.value })}}/>
+                    </Box>
+                </Grid>
+                <Grid xs={12}>
+                    <CustomButton btnText="Sign Up" color="primary" variant="contained" onClick={submitHandler} />
+                </Grid>
+            </Grid>
+        </Box>
     );
 };
 export default SignUp;
