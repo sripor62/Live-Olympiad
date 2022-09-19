645792c0e955730d010fc2c42503bede329e645d
Divesh Singh <digiappstores@gmail.com>
2022-09-19 11:54:13 +0530

    Code setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//Library import statements...
+import React from 'react';
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
+import LoginPage from './pages/auth/login';
+
+
+
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+      </Routes>
+    </Router>
+
+
   );
-}
+
+};
 
 export default App;
