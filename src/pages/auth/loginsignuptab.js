7b26e218f76a230d56c66fec114fc3c87ae9f4aa
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-09-21 10:28:03 +0530

    Update loginsignuptab.js

--- src/pages/auth/loginsignuptab.js
+++ src/pages/auth/loginsignuptab.js
@@ -8,7 +8,7 @@ import SignUp from './SignUp'
 export const LoginSignTabGroup = () => {
     const tabArr = [
         { label: "Signup", component: <SignUp/> },
-        { label: "Login", component: <LoginPage /> },
+        { label: "Login", component: <LoginPage/> },
     ]
 
     return <AuthLayout>
