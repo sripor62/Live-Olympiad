a90211bd6f37dc6518456409560a084b96506fe7
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-09-22 16:20:43 +0530

    Update Dashboard.js

--- src/pages/auth/Dashboard.js
+++ src/pages/auth/Dashboard.js
@@ -85,18 +85,18 @@ const Dashboard = () => {
                                             />
                                         </Box>
                                     </Grid>
-                                    <Grid xs={12} ml={12}>
+                                    <Grid xs={12} ml={14}>
                                         <CustomButton btnText="Upgrade" color="secondary" variant="contained" />
                                     </Grid>
                                 </Grid>
                                 <Grid container xs={12} style={{ border: '2px solid lightGray', borderRadius: '20px', padding: '30px', marginBottom: '20px' }}>
                                     <Grid xs={12}>
-                                        <Typography variant='h4'><Box mb={1} mt={1} ml={2} width={300} align="center">LiveOlympiad Knowledge Tree</Box></Typography>
+                                        <Typography variant='h4'><Box mb={1} mt={1} ml={4} width={300} align="center">LiveOlympiad Knowledge Tree</Box></Typography>
                                     </Grid>
                                     <Grid xs={12}>
-                                        <Typography variant='body2' align="center"><Box mb={3} mt={1}>Learn about Exempler & Challenger Series,Evaluation Criteria.About Live Olympiad,Queries & FAQs,Syllabus through an automated Chatbot. </Box></Typography>
+                                        <Typography variant='body2' align="center"><Box mb={3} mt={1} ml={2} width={350}>Learn about Exempler & Challenger Series,Evaluation Criteria.About Live Olympiad,Queries & FAQs,Syllabus through an automated Chatbot. </Box></Typography>
                                     </Grid>
-                                    <Grid xs={12} ml={12}>
+                                    <Grid xs={12} ml={14}>
                                         <CustomButton btnText="GET STARTED" color="primary" variant="contained" />
                                     </Grid>
                                 </Grid>
