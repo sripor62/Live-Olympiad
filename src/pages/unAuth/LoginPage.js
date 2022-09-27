8d753002c7d037de6e798e3af911d3ca8147b9f8
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-09-27 12:54:10 +0530

    adding header

--- src/pages/unAuth/LoginPage.js
+++ src/pages/unAuth/LoginPage.js
@@ -59,7 +59,7 @@ export default function LoginPage() {
             </Grid>
             <Grid xs={12}>
                 <Box mb={3} width={1} textAlign="right">
-                    <Link href="" underline="none" onClick={() => navigate('/forgotpassword')}>Forgot Password</Link>
+                    <Link href="" underline="none" onClick={() => navigate('/forgotpassword')}>Forgot Password?</Link>
                 </Box>
             </Grid>
             <Grid xs={12}>
--- src/pages/unAuth/layout/UnAuthLayout.js
+++ src/pages/unAuth/layout/UnAuthLayout.js
@@ -13,6 +13,15 @@ export const UnAuthLayout = (props) => {
                         </Grid>
                     </Box>
                 </Grid>
+                <Grid item xs={12} lg={7} sx={{display:{xs:'block',lg:'none'}}}>
+                    <Box width={1} height={0.6} sx={{ backgroundColor: "#4545A5" }}>
+                        <Grid container xs={12} lg={12} sx={{ height: "100%" }} alignItems="center" justifyContent="center">
+                            <Grid item xs={10} lg={10}>
+                                <img src='../images/brand.svg' style={{ width: "150px" }} />
+                            </Grid>
+                        </Grid>
+                    </Box>
+                </Grid>
             <Grid item  xs={12} lg={5}>
                 <Grid container xs={12} lg={12} direction="row" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
                     <Grid item xs={10} lg={10}>
