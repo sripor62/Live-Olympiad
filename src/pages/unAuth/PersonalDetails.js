670403f744f18b8bcf9e8923cab15f1de94e3385
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-09-27 16:09:05 +0530

    updates

--- src/pages/unAuth/PersonalDetails.js
+++ src/pages/unAuth/PersonalDetails.js
@@ -25,7 +25,7 @@ export default function PersonalDetails() {
     return <OnboardingLayout stepperIndex="1">
         <Grid container xs="12">
             <Grid xs={12}>
-                <Typography variant='h3'><Box mb={3} mt={9} sx={{ textAlign: { xs: 'center', lg: 'left' }}}>Personal Details</Box></Typography>
+                <Typography variant='h3'><Box mb={3} mt={5} sx={{ textAlign: { xs: 'center', lg: 'left' }}}>Personal Details</Box></Typography>
             </Grid>
             <Grid xs={12}>
                 <Typography variant='body2'><Box mb={3} sx={{ textAlign: { xs: 'center', lg: 'left' }}}>Enter the personal details of the student </Box></Typography>
