3188a536f99608c7c42204443cd32acd8d6f8e75
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-09-27 13:12:12 +0530

    Update OnboardingLayout.js

--- src/pages/unAuth/layout/OnboardingLayout.js
+++ src/pages/unAuth/layout/OnboardingLayout.js
@@ -6,7 +6,7 @@ export const OnboardingLayout = (props) => {
 
     return <Box sx={{ height: "100vh" }}>
         <Grid container sx={{ height: "100%" }}>
-            <Grid  xs={12} lg={7} sx={{display:{xs:'none',lg:'block'}}}>
+            <Grid xs={12} lg={7} sx={{ display: { xs: 'none', lg: 'block' } }}>
                 <Box width={1} height={1} sx={{ backgroundColor: "#4545A5" }}>
                     <Box width={1} py={10} ml={10}>
                         <img src='../images/brand.svg' style={{ width: "250px" }} />
@@ -15,8 +15,16 @@ export const OnboardingLayout = (props) => {
                     <LoStepper activeStep={props.stepperIndex} />
                 </Box>
             </Grid>
+            <Grid item xs={12} lg={7} sx={{ display: { xs: 'block', lg: 'none' } }}>
+            <Box width={1} height={0.7} sx={{ backgroundColor: "#4545A5" }}>
+                    <Box width={1} py={4} ml={2}>
+                        <img src='../images/brand.svg' style={{ width: "150px" }} />
+                    </Box>
+                </Box>
+            </Grid>
+               
             <Grid xs={12} lg={5}>
-                <Grid container xs={12} lg={12}  direction="row" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
+                <Grid container xs={12} lg={12} direction="row" justifyContent="center" alignItems="center" sx={{ height: "100%" }}>
                     <Grid xs="10">
                         {props.children}
                     </Grid>
