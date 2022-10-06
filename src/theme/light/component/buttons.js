8ba218da5eab76a1761a4469a0deb1e939908e8e
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-10-06 08:29:57 +0530

    Update buttons.js

--- src/theme/light/component/buttons.js
+++ src/theme/light/component/buttons.js
@@ -6,7 +6,9 @@ export const buttonStyle = {
                 padding: "9px 16px !important",
                 '&.minWidth240': {
                     minWidth: "240px !important",
-                    fontWeight:"700"
+                    fontWeight:"700",
+                    fontSize:"20px",
+                    fontFamily:'Urbantist'
                 }
             }
         }
