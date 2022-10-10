64413423123355ec2fe519ef01346e97ae296252
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-10-10 16:48:57 +0530

    Update responsive.js

--- src/theme/responsive.js
+++ src/theme/responsive.js
@@ -24,9 +24,6 @@ export const responsiveStype = {
     Login: {
         Typo: { textAlign: { xs: 'center', lg: 'left' } }
     },
-    Mobilever: {
-        Typo: { textAlign: { xs: 'center', lg: 'left' } }
-    },
     ForgotPassword: {
         Typo: { textAlign: { xs: 'center', lg: 'left' }, fontFamily: "Urbantist" }
     },
