77ee30d52640b1b73cbcdd8b664ccfa154957f0b
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-10-12 15:25:55 +0530

    font change

--- src/pages/auth/MenuDrawer.js
+++ src/pages/auth/MenuDrawer.js
@@ -12,10 +12,10 @@ const MenuDrawer = ({ setDrawerFlag }) => {
                 <Box style={{ minWidth:'250px', width:'70%', maxWidth: '350px', padding: '20px' }}>
                     <Grid container mb={1} style={{ backgroundColor: "#F7F8F9", height: "100%" }}>
                         <Grid container md={2} lg={2} ml={2} mt={4} sx={{ display: { xs: 'block', sm: 'block', md: 'none', lg: 'none' } }}>
-                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><AutoAwesomeMosaicOutlinedIcon /><Typography variant='body2'><Box sx={{ ml: 2 }}>Dashboard </Box></Typography></Grid>
-                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><AccountCircleOutlinedIcon /><Typography><Box sx={{ ml: 2 }}>Profile</Box></Typography></Grid>
-                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><QueuePlayNextOutlinedIcon /><Typography><Box sx={{ ml: 2 }}>Subscriptions</Box></Typography></Grid>
-                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={2}><CalendarMonthOutlinedIcon /><Typography><Box sx={{ ml: 2 }}>Schedule</Box></Typography></Grid>
+                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><AutoAwesomeMosaicOutlinedIcon /><Typography variant='body2'><Box sx={{ ml: 2 ,fontWeight:'bold'}}>Dashboard </Box></Typography></Grid>
+                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><AccountCircleOutlinedIcon /><Typography><Box sx={{ ml: 2,fontWeight:'bold' }}>Profile</Box></Typography></Grid>
+                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={3}><QueuePlayNextOutlinedIcon /><Typography><Box sx={{ ml: 2,fontWeight:'bold' }}>Subscriptions</Box></Typography></Grid>
+                            <Grid xs={12} sm={12} md={12} lg={12} style={{ display: 'flex', alignItems: 'center' }} mb={2}><CalendarMonthOutlinedIcon /><Typography><Box sx={{ ml: 2,fontWeight:'bold' }}>Schedule</Box></Typography></Grid>
                         </Grid>
                     </Grid>
                 </Box>
