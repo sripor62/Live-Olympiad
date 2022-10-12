283fe313d8cfd3f0b85dd8bf6c066165db5d647b
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-10-12 15:40:10 +0530

    Update Dashboard.js

--- src/pages/auth/Dashboard.js
+++ src/pages/auth/Dashboard.js
@@ -57,10 +57,10 @@ const Dashboard = () => {
                 <Grid container mb={5} xs={12} sm={12} md={12} lg={12} alignItems='center'>
                     <Grid xs={12} sm={9} md={8} lg={9} mt={2}>
                         <Stack direction="row" spacing={2}>
-                            <Chip label="All" variant="outlined" sx={{ color: '#ffffff', border: '2px solid #6A707C', backgroundColor: '#6A707C' }} />
-                            <Chip label="Science" variant="outlined" sx={{ color: '#42BBEF', border: '2px solid #42BBEF' }} />
-                            <Chip label="Mathematics" variant="outlined" sx={{ color: '#4545A5', border: '2px solid #4545A5' }} />
-                            <Chip label="English" variant="outlined" sx={{ color: '#EF4255', border: '2px solid #EF4255' }} />
+                            <Chip label="All" variant="outlined" sx={{ color: '#ffffff', border: '2px solid #6A707C', backgroundColor: '#6A707C',fontWeight:600,fontSize:'12px' }} />
+                            <Chip label="Science" variant="outlined" sx={{ color: '#42BBEF', border: '2px solid #42BBEF',fontWeight:600 ,fontSize:'12px' }} />
+                            <Chip label="Mathematics" variant="outlined" sx={{ color: '#4545A5', border: '2px solid #4545A5',fontWeight:600,fontSize:'12px'  }} />
+                            <Chip label="English" variant="outlined" sx={{ color: '#EF4255', border: '2px solid #EF4255',fontWeight:600,fontSize:'12px'  }} />
                         </Stack>
                     </Grid>
                     <Grid xs={12} sm={3} md={4} lg={3} mt={2}>
@@ -87,13 +87,13 @@ const Dashboard = () => {
                             <Chip label="Science" color="primary" className='brb_0' />
                             <Grid container item xs={12} sm={12} md={12} lg={12} style={{ backgroundColor: '#F7F8F9', borderRadius: '0px 20px 20px 20px', padding: '20px', marginBottom: '20px' }}>
 
-                                <Grid item xs={4} sm={6} md={6} lg={5} style={{ justifyContent: 'flex-start', alignItems: 'center' }}>
+                                <Grid item xs={4} sm={6} md={6} lg={4} style={{ justifyContent: 'flex-start', alignItems: 'center' }}>
                                     <Typography variant='body1'><Box mb={1} sx={{fontFamily:'Urbantist',fontSize:"18px",fontWeight:600}}>Test Name</Box></Typography>
                                     <Typography variant='body2'><Box mb={1} sx={{fontFamily:'Urbantist',fontWeight:500,color:'rgba(106,112,124,1)'}}>Practice Test</Box></Typography>
                                 </Grid>
-                                <Grid item xs={8} sm={6} md={6} lg={7} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+                                <Grid item xs={8} sm={6} md={6} lg={8} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
                                 <Typography sx={{ fontSize: "18px", display: 'flex'}} ><Box sx={{ fontSize: '50px', color: 'green' }}>19</Box><Box sx={{ mt: 4 ,mr:4}}>/20</Box></Typography>
-                                    <CustomButton btnText="RE-TAKE" endIcon={<RefreshIcon />} variant="contained" />
+                                    <CustomButton btnText="RE-TAKE" endIcon={<RefreshIcon />} variant="contained" sx={{width:'104px',height:'36px',fontSize:'12px'}}/>
                                 </Grid>
                             </Grid>
                         </Box>
@@ -107,17 +107,17 @@ const Dashboard = () => {
                     </Grid>
                     <Grid item md={12} lg={12} sx={{borderRadius:"10px",border:"1px solid #4545A5"}} mb={1}>
                         <Box>
-                            <Typography p={1} color=" #4545A5">Mathematics</Typography>   
+                            <Typography p={1} color=" #4545A5" fontFamily="Urbanist" fontWeight={600} fontSize="16px">Mathematics</Typography>   
                         </Box>
                     </Grid>
                     <Grid item md={12} lg={12} sx={{borderRadius:"10px",border:"1px solid #42BBEF"}} mb={1}>
                         <Box>
-                            <Typography p={1} color="#42BBEF">Science</Typography>   
+                            <Typography p={1} color="#42BBEF" fontFamily="Urbanist" fontWeight={600} fontSize="16px">Science</Typography>   
                         </Box>
                     </Grid>
                     <Grid item md={12} lg={12} sx={{borderRadius:"10px",border:"1px solid red"}} mb={2}>
                         <Box>
-                            <Typography p={1} color="red">English</Typography>   
+                            <Typography p={1} color="red" fontFamily="Urbanist" fontWeight={600} fontSize="16px">English</Typography>   
                         </Box>
                     </Grid>
                     <Grid item md={12} lg={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
