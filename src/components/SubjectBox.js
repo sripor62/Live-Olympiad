6344591272416a7d14273cc8e0b40405820c0307
Arsh <arshdeepsp@gmail.com>
2022-10-15 17:10:21 -0700

    Componentization 1

---
+++ src/components/SubjectBox.js
@@ -0,0 +1,10 @@
+import { Grid, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+
+export const SubjectBox = (props) => {
+    return <Grid item md={12} lg={12} sx={{borderRadius: props.borderRadius, border: props.border}} mb={1}>
+        <Box>
+            <Typography p={1} color={props.color} fontFamily="Urbanist" fontWeight={600} fontSize="16px">{props.text}</Typography>   
+        </Box>
+    </Grid>
+}
---
+++ src/components/TestBox.js
@@ -0,0 +1,21 @@
+import { Chip, Grid, Typography } from "@mui/material"
+import { Box } from "@mui/system"
+import { CustomButton } from "./Button"
+import RefreshIcon from '@mui/icons-material/Refresh';
+
+export const TestBox = () => {
+    return <Box>
+        <Chip label="Science" color="primary" className='brb_0' />
+        <Grid container item xs={12} sm={12} md={12} lg={12} style={{ backgroundColor: '#F7F8F9', borderRadius: '0px 20px 20px 20px', padding: '20px', marginBottom: '20px' }}>
+
+            <Grid item xs={4} sm={6} md={6} lg={4} style={{ justifyContent: 'flex-start', alignItems: 'center' }}>
+                <Typography variant='body1'><Box mb={1} sx={{fontFamily:'Urbantist',fontSize:"18px",fontWeight:600}}>Test Name</Box></Typography>
+                <Typography variant='body2'><Box mb={1} sx={{fontFamily:'Urbantist',fontWeight:500,color:'rgba(106,112,124,1)'}}>Practice Test</Box></Typography>
+            </Grid>
+            <Grid item xs={8} sm={6} md={6} lg={8} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
+            <Typography sx={{ fontSize: "18px", display: 'flex'}} ><Box sx={{ fontSize: '50px', color: 'green' }}>19</Box><Box sx={{ mt: 4 ,mr:4}}>/20</Box></Typography>
+                <CustomButton btnText="RE-TAKE" endIcon={<RefreshIcon />} variant="contained"  sx={{width:'105px',height:'36px',fontSize:'12px',backgroundColor:"#838BA1" ,fontWeight:600}}/>
+            </Grid>
+        </Grid>
+    </Box>
+}
