7c7e242a8d5ca83480cc4de4f6a4043a7834dfa6
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-10-17 16:49:05 +0530

    Update SignUp.js

--- src/pages/Auth/SignUp.js
+++ src/pages/Auth/SignUp.js
@@ -38,7 +38,7 @@ const SignUp = () => {
         <Box>
             <Grid container>
                 <Grid xs={12} >
-                    <Typography variant='h3'><Box mb={2} mt={6} fontWeight="bold" sx={responsiveStype.Signup.Typo}>Sign Up</Box></Typography>
+                    <Typography variant='h3'><Box mb={2} mt={6} fontWeight="bold" fontSize="40px" sx={responsiveStype.Signup.Typo}>Sign Up</Box></Typography>
                 </Grid>
                 <Grid xs={12}>
                     <Typography variant='body2' color="#838BA1" fontSize="14px"  fontWeight={600}><Box mb={3} sx={responsiveStype.Signup.Typo}>New User? Enter your phone number below.</Box></Typography>
