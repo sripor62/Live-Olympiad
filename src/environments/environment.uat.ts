9265bd314c30836bc5aa8336270c4584cc9e4094
Onkar Parmar <onkar.parmar@gmail.com>
2022-10-21 14:02:22 +0200

    uat env

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -3,7 +3,7 @@ export const environment = {
     userBaseUrl: "https://users.aimlearn.co",
     domain: "liveolympiad.org",
     cookieDomain: ".liveolympiad.org",
-    authUrl: "http://localhost:3000",
-    learnerUrl: "http://localhost:3001",
+    authUrl: "https://uat.liveolympiad.org",
+    learnerUrl: "https://uat.liveolympiad.org",
   };
   
