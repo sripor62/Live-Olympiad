6a52511a25d50ab566aa0cf6a2af72d20e283b74
Arsh <arshdeepsp@gmail.com>
2022-10-26 01:59:51 -0700

    Minor UI changes

--- src/components/CustomListItem.js
+++ src/components/CustomListItem.js
@@ -14,7 +14,7 @@ export const CustomListItem = (props) => {
             </Grid>
             <Grid item xs={8} sm={6} md={6} lg={8} style={{ display: 'flex', justifyContent: 'flex-end', alignItems: 'center' }}>
 
-            <Typography sx={{ fontSize: "18px", display: 'flex'}} ><Box sx={{ fontSize: '50px', color: 'green' }}>19</Box><Box sx={{ mt: 4 ,mr:4}}>/{props.option.totalMarks}</Box></Typography>
+            <Typography sx={{ fontSize: "18px", display: 'flex'}} ><Box sx={{ fontSize: '50px', color: 'green' }}>9</Box><Box sx={{ mt: 4 ,mr:4}}>/{props.option.totalMarks}</Box></Typography>
                 <CustomButton btnText="RE-TAKE" endIcon={<RefreshIcon/>} variant="contained"  sx={{width:'105px',height:{xs:"34px",lg:'36px'},fontSize:{xs:"8px",lg:'12px'},backgroundColor:"#838BA1" ,fontWeight:600}}/>
 
             </Grid>
