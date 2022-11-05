b330699c7f5c707ac63da65aa217e8842cf27b6b
Onkar Parmar <onkar@Digishaala.local>
2022-11-05 06:12:02 +0100

    live env

---
+++ src/environments/environment.live.ts
@@ -0,0 +1,9 @@
+export const environment = {
+    production: false,
+    userBaseUrl: "https://users.aimlearn.co",
+    domain: "liveolympiad.org",
+    cookieDomain: ".liveolympiad.org",
+    paymentUrl:"https://payments.aimlearn.co",
+    testLiveolympiad:"https://api.liveolympiad.org"
+  };
+  
