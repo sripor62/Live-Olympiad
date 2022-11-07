4efa4e20705c404453ef17c08cb35c5e85cf382d
Srishti Porwal <srishtiporwal120202@gmail.com>
2022-11-07 18:49:36 +0530

    Filter

--- src/designs/Dashboard/DashboardLayout.js
+++ src/designs/Dashboard/DashboardLayout.js
@@ -11,7 +11,7 @@ export const DashboardLayout = (props) => {
         <Grid item p={2} xs={12} sm={12} md={8} lg={7}>
             <Grid container mb={5}  alignItems='center'>
                 <Grid item xs={12} md={12} lg={8}><SubjectSelector setPage={props.setPage} /></Grid>
-                { <Grid item  xs={12} md={12} lg={4}><FilterByStatus open={props.open} anchorEl={props.anchorEl} handleClick={props.handleClick} handleClose={props.handleClose} /></Grid> }
+                { <Grid item  xs={12} md={12} lg={4}><FilterByStatus setPage={props.setPage} open={props.open} anchorEl={props.anchorEl} handleClick={props.handleClick} handleClose={props.handleClose} /></Grid> }
             </Grid>
             {props.page===0 &&
                 props?.testsLists?.map((option) => {
@@ -30,6 +30,21 @@ export const DashboardLayout = (props) => {
             }
             {
                 props.page===3 && props?.testsLists.filter((item)=> {return item.subject[0]==="English"}).map((option) => {
+                    return <CustomListItem option={option} key={option._id} testScreen={props.testScreen}/>
+                })
+            }
+            {
+                props.page===4 && props?.testsLists.filter((item)=> {return item.testStatus && item.attemptedQuestions===item.totalMarks}).map((option) => {
+                    return <CustomListItem option={option} key={option._id} testScreen={props.testScreen}/>
+                })
+            }
+            {
+                props.page===5 && props?.testsLists.filter((item)=> {return item.testStatus && item.attemptedQuestions!==item.totalMarks}).map((option) => {
+                    return <CustomListItem option={option} key={option._id} testScreen={props.testScreen}/>
+                })
+            }
+            {
+                props.page===6 && props?.testsLists.filter((item)=> {return item.testStatus==false || item.testStatus===undefined}).map((option) => {
                     return <CustomListItem option={option} key={option._id} testScreen={props.testScreen}/>
                 })
             }
