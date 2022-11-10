652c8abdde7d8c477ee33405773e69272598c7a3
praneeth_reddy513 <praneeth.reddy@liqvid.com>
2022-11-10 11:59:17 +0530

    refac

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -3,19 +3,19 @@ import { environment } from "../environments/environment";
 import jwt_decode from "jwt-decode";
 
 export function navigateAsPerSessionValidity(authenticate) {
-  let userInfoVal=""
-  var userInfo={}
-    if(window.localStorage.getItem("current_user")){console.log("user",window.localStorage.getItem("current_user"))
-    userInfoVal=window.localStorage.getItem("current_user")
-     var userInfo = JSON.parse(userInfoVal).state.currentUser;
-    }
+  let userInfoVal = "";
+  var userInfo = {};
+  if (window.localStorage.getItem("current_user")) {
+    userInfoVal = window.localStorage.getItem("current_user");
+    var userInfo = JSON.parse(userInfoVal).state.currentUser;
+  }
   if (userInfo) {
     // const userInfo = JSON.parse(userInfoVal);
     let decodedToken = jwt_decode(userInfo.access_token);
-   
+
     let currentDate = new Date();
-   
-    if (decodedToken.exp * 1000 >= currentDate.getTime()) {    
+
+    if (decodedToken.exp * 1000 >= currentDate.getTime()) {
       if (userInfo.userRole === "STUDENT") {
         if (!authenticate) {
           // window.location.href="/dashboard";
@@ -27,6 +27,6 @@ export function navigateAsPerSessionValidity(authenticate) {
     }
   }
   if (authenticate) {
-    window.location.href="/"; 
+    window.location.href = "/";
   }
 }
