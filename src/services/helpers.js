50c5d7a4886c5bc27b4f8569fd3cee380e5361cb
praneeth_reddy513 <praneeth.reddy@liqvid.com>
2022-11-10 11:54:46 +0530

    refac

--- src/services/helpers.js
+++ src/services/helpers.js
@@ -3,17 +3,13 @@ import { environment } from "../environments/environment";
 import jwt_decode from "jwt-decode";
 
 export function navigateAsPerSessionValidity(authenticate) {
-  const cookieServiceValue_USER_INFO = document.cookie.split(";").filter((item)=> item.trim().startsWith(AppConstants.SESSION_STORAGE_ITEMS.USER_INFO));
   let userInfoVal=""
   var userInfo={}
-    // userInfoVal =cookieServiceValue_USER_INFO[0].trim().split("=")[1];
     if(window.localStorage.getItem("current_user")){console.log("user",window.localStorage.getItem("current_user"))
     userInfoVal=window.localStorage.getItem("current_user")
      var userInfo = JSON.parse(userInfoVal).state.currentUser;
     }
-  console.log(userInfo)
   if (userInfo) {
-    console.log("userInfo",userInfo)
     // const userInfo = JSON.parse(userInfoVal);
     let decodedToken = jwt_decode(userInfo.access_token);
    
