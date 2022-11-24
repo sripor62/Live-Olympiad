632712402b26c8f4154af05212a8e504e1ae5a25
deepanjaliparmar <104496749+deepanjaliparmar@users.noreply.github.com>
2022-11-24 14:53:18 +0530

    some new feature
    add booklet
    add name in avatar in header and profile
    change in test player

--- src/designs/Dashboard/UserDetails.js
+++ src/designs/Dashboard/UserDetails.js
@@ -1,6 +1,44 @@
 import { CurrencyRupee } from "@mui/icons-material"
 import { Avatar, Box, Grid, Typography } from "@mui/material"
 import { propsToClassKey } from "@mui/styles"
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+  
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+      hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+  
+    let color = '#';
+  
+    for (i = 0; i < 3; i += 1) {
+      const value = (hash >> (i * 8)) & 0xff;
+      color += `00${value.toString(16)}`.slice(-2);
+    }
+    /* eslint-enable no-bitwise */
+  
+    return color;
+  }
+  function stringAvatar(name) {
+    if(name.split(' ')[0] && name.split(' ')[1]){
+    return {
+      sx: {
+        bgcolor: stringToColor(name),
+        width: '133px', height: '133px'
+      },
+      children: `${name.split(' ')[0][0]}${name.split(' ')[1][0]}`,
+    };}
+    else if(name.split(' ')[0][0]){
+        return {
+            sx: {
+              bgcolor: stringToColor(name),
+              width: '133px', height: '133px'
+            },
+            children: `${name.split(' ')[0][0]}`,
+          };
+    }
+  }  
 
 export const UserDetails = (props) => {
     return <>
@@ -9,7 +47,7 @@ export const UserDetails = (props) => {
                 <Grid container>
                     <Grid item xs={12} sm={12} md={12} lg={12} mb={3} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
                         <Box sx={{ width: '100%', height: '100%', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-                            <Avatar sx={{ width: '133px', height: '133px'}} />
+                            <Avatar  {...stringAvatar(props.pageData?.fullName)}/>
                         </Box>
                     </Grid>
                     <Grid item xs={12} sm={12} md={12} lg={12} sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
---
+++ src/pages/Dashboard/Booklet.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import BookletLayout from '../../designs/Dashboard/BookletLayout'
+import HomeLayout from '../../designs/Dashboard/HomeLayout';
+import { useStore } from "../../stores";
+
+const Booklet = () => {
+    const clearCurrentUser = useStore((state) => state.clearCurrentUser)
+  return (
+    <HomeLayout logOutHandler={clearCurrentUser}><BookletLayout/></HomeLayout>
+  )
+}
+
+export default Booklet
