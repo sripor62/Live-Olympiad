aa59390288f5faa8bfdabb6ce88e2fce3881f903
felle3232 <feleenrani@gmail.com>
2023-04-27 00:23:05 +0530

    update

--- src/parentlog/index.js
+++ src/parentlog/index.js
@@ -1,12 +1,29 @@
-import { Avatar, Box, Button, Grid, Typography } from "@mui/material";
+import { Avatar, Box, Button, Grid, Pagination, Typography } from "@mui/material";
 import { responsiveStype } from "../beautifiers/responsive";
 import { AuthLayout } from "../designs/Auth/AuthLayout";
 import { AddCircle } from "@mui/icons-material";
+import { useState } from "react";
 
-const children =[{id: 1, name: "Aneysha Das", school: "Cambridge School", class: "V", section: "B" },  
-                 { id: 2, name: "John Doe", school: "St. Mary's School", class: "VI", section: "A" },]
+const childrenData =[{id: 1, name: "Aneysha Das", school: "Cambridge School", class: "V", section: "B" },  
+                    { id: 2, name: "John Doe", school: "St. Mary's School", class: "VI", section: "A" },
+                    { id: 3, name: "James", school: "Oxford", class: "IV", section: "C" },
+                    { id: 4, name: "Tom Brown", school: "Harvard School", class: "III", section: "D" },
+                    { id: 5, name: "Sarah Lee", school: "Stanford School", class: "IV", section: "A" },
+                    { id: 6, name: "Peter Parker", school: "Midtown School of Science and Technology", class: "IX", section: "B" },
+                    { id: 7, name: "Bruce Wayne", school: "Gotham Academy", class: "X", section: "C" },
+                    { id: 8, name: "Tony Stark", school: "MIT", class: "XI", section: "D" },
+                    ]
 
 const ParentPage = () => {
+    const [page, setPage] = useState(1);
+    const PAGE_SIZE = 2;
+    const startIndex =(page - 1) * PAGE_SIZE;
+    const endIndex = startIndex + PAGE_SIZE;
+
+    const handlePageChange = (event, value) => {
+        setPage(value);
+    }
+    const children = childrenData.slice(startIndex, endIndex);
     return(
     <AuthLayout responsiveStype={responsiveStype}>
         <Box sx={{marginTop:'-10%',marginLeft:'-18%'}}>
@@ -14,9 +31,10 @@ const ParentPage = () => {
                         fontSize:'22px',lineHeight:'26px',color:'#060606'}}>
             My Childrens</Typography> 
         </Box>
-        <Grid container spacing={0.5}sx={{ marginTop: "10px", marginLeft: "18%" ,justifyContent:'space-between' }}>
-        {children.map((child)=>(
-            <Box key={child.id}
+        <Grid container spacing={2} sx={{marginTop:'6%',display:'flex',justifyContent:'space-between'}} >
+        {children.map((child)=>(  
+            <Grid key={child.id}  > 
+            <Box 
             sx={{backgroundColor:'#4545A5',width:'195px',
             height:'108px',
             borderRadius:'8px',boxShadow:'0px 6px 15px rgba(0, 0, 0, 0.25)',
@@ -56,9 +74,15 @@ const ParentPage = () => {
                    </Button>
                 
             </Box>
-    </Box>
+          </Box>
+        </Grid> 
         ))}
         </Grid>
+        <Pagination 
+        count={Math.ceil(childrenData.length/PAGE_SIZE)}
+        page={page}
+        onChange={handlePageChange}
+        sx={{mt:2}}/>
         <Box sx={{width:'97px',height:'45px',color:'#FFFFFF',backgroundColor:'#F9BB47',boxShadow:'0px 4px 11px rgba(0, 0, 0, 0.25)',
                 borderRadius:'10px', marginTop:'10%',marginLeft:'6%',justifyContent:'center',alignItems:'center',display:'flex',flexDirection:'column'}}>
             <AddCircle />
