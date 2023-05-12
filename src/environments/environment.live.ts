65963fb07f6a9cfa26ba23b6c060a285d6a6b4e0
Nidhi Parmar <nidhi@OnkarMBP.local>
2023-05-12 20:13:35 +0200

    fix env

--- src/environments/environment.live.ts
+++ src/environments/environment.live.ts
@@ -1,6 +1,8 @@
 export const environment = {
   userBaseUrl: "https://users.digishaala.com",
   domain: "liveolympiad.org",
+  studentsBaseUrl: " https://ouk5lpzn0i.execute-api.ap-south-1.amazonaws.com/live/students",
+  schoolsBaseUrl: "https://qpqinf8e1h.execute-api.ap-south-1.amazonaws.com/live/schools",
   cookieDomain: ".liveolympiad.org",
   paymentUrl:"https://payments.digishaala.com",
   testLiveolympiad:"https://content.liveolympiad.org",
