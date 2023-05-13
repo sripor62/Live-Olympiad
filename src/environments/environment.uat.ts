21221f2d58989c6f1cace057ef788624a412064e
praneeth_reddy513 <praneeth.reddy@liqvid.com>
2023-05-13 12:32:41 +0530

    space removed

--- src/environments/environment.uat.ts
+++ src/environments/environment.uat.ts
@@ -1,7 +1,7 @@
 export const environment = {
     production: false,
     userBaseUrl: "https://users-uat.digishaala.com",
-    studentsBaseUrl: " https://2fjloypeob.execute-api.ap-south-1.amazonaws.com/uat/students",
+    studentsBaseUrl: "https://2fjloypeob.execute-api.ap-south-1.amazonaws.com/uat/students",
     schoolsBaseUrl: "https://oug6u98n2h.execute-api.ap-south-1.amazonaws.com/uat/schools",
     domain: "liveolympiad.org",
     cookieDomain: ".liveolympiad.org",
