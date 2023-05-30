36cb429103a27889c6c24ec88c89d063f00e7356
Manan <manandude02@gmail.com>
2023-05-30 11:10:33 +0530

    Fixed bug REG-40

--- src/pages/ParentView.js
+++ src/pages/ParentView.js
@@ -5,55 +5,55 @@ import {
 	Grid,
 	IconButton,
 	Typography,
-} from "@mui/material";
-import { responsiveStype } from "../beautifiers/responsive";
-import { AuthLayout } from "../designs/Auth/AuthLayout";
-import { AddCircle } from "@mui/icons-material";
-import { useEffect, useState } from "react";
-import { useNavigate } from "react-router-dom";
-import { CustomButton } from "../components/CustomButton";
-
-import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
-import ChevronRightIcon from "@mui/icons-material/ChevronRight";
-import { useStore } from "../stores";
-import { useStudent } from "../hooks/useStudent";
-import { useQuery } from "react-query";
-import { useSchool } from "../hooks/useSchool";
-import { ParentLog } from "../designs/ParentLog/ParentLog";
-
-const childrenData = [
+  } from "@mui/material";
+  import { responsiveStype } from "../beautifiers/responsive";
+  import { AuthLayout } from "../designs/Auth/AuthLayout";
+  import { AddCircle } from "@mui/icons-material";
+  import { useEffect, useState } from "react";
+  import { useNavigate } from "react-router-dom";
+  import { CustomButton } from "../components/CustomButton";
+  
+  import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+  import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+  import { useStore } from "../stores";
+  import { useStudent } from "../hooks/useStudent";
+  import { useQuery } from "react-query";
+  import { useSchool } from "../hooks/useSchool";
+  import { ParentLog } from "../designs/ParentLog/ParentLog";
+  
+  const childrenData = [
 	{
-		id: 1,
-		name: "Aneysha Das",
-		school: "Cambridge School",
-		class: "V",
-		section: "B",
+	  id: 1,
+	  name: "Aneysha Das",
+	  school: "Cambridge School",
+	  class: "V",
+	  section: "B",
 	},
 	{
-		id: 2,
-		name: "John Doe",
-		school: "St. Mary's School",
-		class: "VI",
-		section: "A",
+	  id: 2,
+	  name: "John Doe",
+	  school: "St. Mary's School",
+	  class: "VI",
+	  section: "A",
 	},
 	{ id: 3, name: "James", school: "Oxford", class: "IV", section: "C" },
 	{
-		id: 4,
-		name: "Tom Brown",
-		school: "Harvard School",
-		class: "III",
-		section: "D",
+	  id: 4,
+	  name: "Tom Brown",
+	  school: "Harvard School",
+	  class: "III",
+	  section: "D",
 	},
 	{
-		id: 4,
-		name: "Tom Cruise",
-		school: "Harvard School",
-		class: "II",
-		section: "E",
+	  id: 5,
+	  name: "Tom Cruise",
+	  school: "Harvard School",
+	  class: "II",
+	  section: "E",
 	},
-];
-
-const ParentView = ({ logOutHandler }) => {
+  ];
+  
+  const ParentView = ({ logOutHandler }) => {
 	const navigate = useNavigate();
 	const currentUser = useStore((state) => state.currentUser);
 	const clearCurrentUser = useStore((state) => state.clearCurrentUser);
@@ -65,63 +65,57 @@ const ParentView = ({ logOutHandler }) => {
 	const startIndex = (page - 1) * PAGE_SIZE;
 	const endIndex = startIndex + PAGE_SIZE;
 	const numPages = Math.ceil(childrenData.length / PAGE_SIZE);
+  
 	const handleClickPrev = () => {
-		setPage((prevIndex) => prevIndex - 1);
+	  setPage((prevIndex) => Math.max(prevIndex - 1, 1));
 	};
-
+  
 	const handleClickNext = () => {
-		setPage((prevIndex) => prevIndex + 1);
+	  setPage((prevIndex) => Math.min(prevIndex + 1, numPages));
 	};
-
+  
 	const { data: students } = useQuery(
-		[`students`, currentUser],
-		() => getStudentsOfUser(currentUser?.id),
-		{ enabled: !!currentUser?.id, retry: false }
+	  [`students`, currentUser],
+	  () => getStudentsOfUser(currentUser?.id),
+	  { enabled: !!currentUser?.id, retry: false }
 	);
-	var schoolMap = {};
-	const {
-		data: schoolsData,
-		isLoading: schoolsLoader,
-		refetch: schoolsFetch,
-	} = useQuery([`SchoolsData`], () => getSchools(), {
-		enabled: true,
-		retry: false,
-	});
-
+  
 	useEffect(() => {
-		if (students?.data?.data) {
-			setChildren(students?.data?.data?.slice(startIndex, endIndex));
-		}
-	}, [students]);
-
+	  if (students?.data?.data) {
+		setChildren(students?.data?.data?.slice(startIndex, endIndex));
+	  }
+	}, [students, startIndex, endIndex]);
+  
 	const onHandleClickCreate = async () => {
-		let res = await profileDataDetails({
-			userId: currentUser?.id,
-		});
-		if (res?.data?.success) {
-			navigate("/schooldetails/" + res?.data?.data?._id);
-		}
+	  let res = await profileDataDetails({
+		userId: currentUser?._id,
+	  });
+	  if (res?.data?.success) {
+		navigate("/schooldetails/" + res?.data?.data?._id);
+	  }
 	};
-
+  
 	const onHandleClickNavigate = async (child) => {
-		sessionStorage.setItem("current_student", JSON.stringify(child));
-		navigate("/dashboard");
+	  sessionStorage.setItem("current_student", JSON.stringify(child));
+	  navigate("/dashboard");
 	};
-
+  
 	return (
-	<ParentLog
-	handleClickNext={handleClickNext}
-	responsiveStype={responsiveStype}
-	logOutHandler={clearCurrentUser}
-	page={page}
-	setPage={setPage}
-	handleClickPrev={handleClickPrev}
-	numPages={numPages}
-	schoolsData={schoolsData}
-	onHandleClickNavigate={onHandleClickNavigate}
-	onHandleClickCreate={onHandleClickCreate}
-	children={children}
-	/>
+	  <ParentLog
+		handleClickNext={handleClickNext}
+		handleClickPrev={handleClickPrev}
+		responsiveStyle={responsiveStype}
+		logOutHandler={clearCurrentUser}
+		page={page}
+		setPage={setPage}
+		numPages={numPages}
+		schoolsData={childrenData}
+		onHandleClickNavigate={onHandleClickNavigate}
+		onHandleClickCreate={onHandleClickCreate}
+		children={children}
+	  />
 	);
-};
-export default ParentView;
+  };
+  
+  export default ParentView;
+  
